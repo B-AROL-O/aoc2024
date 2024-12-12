0b0fe7ac41459fde80d79684bac0400a92917881
Alessio Chessa <ale.chessa28@gmail.com>
2024-12-12 23:04:01 +0100

    day 12 part 2 solution

--- solutions/alessio/day12/day12.go
+++ solutions/alessio/day12/day12.go
@@ -14,12 +14,12 @@ func check(e error) {
 	}
 }
 
-func checkInBounds(cell Cell, rows int, cols int) bool {
-	return cell.r >= 0 && cell.r < rows && cell.c >= 0 && cell.c < cols
-}
-
 type Cell struct {
 	r, c int
+}
+
+func checkInBounds(cell Cell, rows int, cols int) bool {
+	return cell.r >= 0 && cell.r < rows && cell.c >= 0 && cell.c < cols
 }
 
 func dfs(curr Cell, lines []string, rows int, cols int, visited *map[Cell]bool, region *[]Cell) {
@@ -60,49 +60,34 @@ const (
 	W
 )
 
-type vertKey struct {
-	col, dir int
-}
-
-type horizKey struct {
-	row, dir int
+type sideKey struct {
+	row, col, dir int
 }
 
 func countSides(r []Cell, grid []string, rows int, cols int) int {
 	symbol := grid[r[0].r][r[0].c]
 	dirs := [][]int{{-1, 0}, {0, 1}, {1, 0}, {0, -1}} // N, E, S, W
-	vertSides := make(map[vertKey][]int)
-	horizSides := make(map[horizKey][]int)
+	sides := make(map[sideKey][]int)
 	for _, cell := range r {
 		for d, dir := range dirs {
 			next := Cell{cell.r + dir[0], cell.c + dir[1]}
 			if !checkInBounds(next, rows, cols) || grid[next.r][next.c] != symbol {
 				// perimetral cell
 				if d == N {
-					horizSides[horizKey{cell.r, d}] = append(horizSides[horizKey{cell.r, d}], cell.c)
+					sides[sideKey{cell.r, -1, d}] = append(sides[sideKey{cell.r, -1, d}], cell.c)
 				} else if d == S {
-					horizSides[horizKey{cell.r + 1, d}] = append(horizSides[horizKey{cell.r + 1, d}], cell.c)
+					sides[sideKey{cell.r + 1, -1, d}] = append(sides[sideKey{cell.r + 1, -1, d}], cell.c)
 				} else if d == W {
-					vertSides[vertKey{cell.c, d}] = append(vertSides[vertKey{cell.c, d}], cell.r)
+					sides[sideKey{-1, cell.c, d}] = append(sides[sideKey{-1, cell.c, d}], cell.r)
 				} else {
-					vertSides[vertKey{cell.c + 1, d}] = append(vertSides[vertKey{cell.c + 1, d}], cell.r)
+					sides[sideKey{-1, cell.c + 1, d}] = append(sides[sideKey{-1, cell.c + 1, d}], cell.r)
 				}
 			}
 		}
 	}
 
 	cnt := 0
-	for _, v := range horizSides {
-		sort.Ints(v)
-		for i := 1; i < len(v); i++ {
-			if v[i]-v[i-1] > 1 {
-				cnt++
-			}
-		}
-		cnt++
-	}
-
-	for _, v := range vertSides {
+	for _, v := range sides {
 		sort.Ints(v)
 		for i := 1; i < len(v); i++ {
 			if v[i]-v[i-1] > 1 {
@@ -117,29 +102,25 @@ func countSides(r []Cell, grid []string, rows int, cols int) int {
 
 func solve(grid []string, useSides bool) {
 	visited := make(map[Cell]bool)
-	regions := make([][]Cell, 0)
 	rows, cols := len(grid), len(grid[0])
-	for r := range rows {
-		for c := range cols {
+	res := 0
+
+	for r := 0; r < rows; r++ {
+		for c := 0; c < cols; c++ {
 			if !visited[Cell{r, c}] {
-				region := make([]Cell, 0)
+				region := []Cell{}
 				dfs(Cell{r, c}, grid, rows, cols, &visited, &region)
-				regions = append(regions, region)
+				area := len(region)
+				perimeter := 0
+				if !useSides {
+					perimeter = computePerimeter(region, grid, rows, cols)
+				} else {
+					perimeter = countSides(region, grid, rows, cols)
+					// fmt.Printf("- A region of %c plants with price %d * %d = %d\n", grid[region[0].r][region[0].c], area, perimeter, area*perimeter)
+				}
+				res += area * perimeter
 			}
 		}
-	}
-
-	res := 0
-	for _, r := range regions {
-		area := len(r)
-		perimeter := 0
-		if !useSides {
-			perimeter = computePerimeter(r, grid, rows, cols)
-		} else {
-			perimeter = countSides(r, grid, rows, cols)
-			fmt.Printf("- A region of %c plants with price %d * %d = %d\n", grid[r[0].r][r[0].c], area, perimeter, area*perimeter)
-		}
-		res += area * perimeter
 	}
 
 	fmt.Println(res)
@@ -154,7 +135,7 @@ func part2(grid []string) {
 }
 
 func main() {
-	data, err := os.ReadFile("./input12.txt")
+	data, err := os.ReadFile("./input12_def.txt")
 	check(err)
 	dataStr := strings.ReplaceAll(string(data), "\r\n", "\n")
 	lines := strings.Split(strings.Trim(dataStr, "\n"), "\n")
